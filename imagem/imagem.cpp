dd4fecda876ebdf83c7b064809ce1810d79537ca
D4nielS4ntos <104071717+D4nielS4ntos@users.noreply.github.com>
2025-07-08 17:32:42 -0300

    criação de imagens PPM

---
+++ imagem/imagem.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+// #include "imagem.h"
+#include "../paleta/paleta.cpp"
+using namespace std;
+
+class Imagem {
+    int largura;
+    int altura;
+    Cor** pixel;
+    int max;
+    
+    public:
+        string tipo;
+
+        Imagem(int alargura, int aaltura, string atipo="P3", int mInt=255) {
+            largura = alargura;
+            altura = aaltura;
+            tipo = atipo;
+            max = mInt;
+        }
+
+        void matrizImagem() {
+            pixel = new Cor*[largura];
+            for (int a = 0; a < largura; a++) {
+                pixel[a] = new Cor[altura];
+                
+                for (int b = 0; b < altura; b++) {
+                    Cor p {0, 0, 0};
+                    pixel[a][b] = p;
+                }
+            }
+        }
+
+        void desalocarMatriz() {
+            for (int c = 0; c < largura; c++){
+                delete[] pixel[c];
+            }
+            delete[] pixel;
+        }
+
+        Cor consultarPixel(int x, int y) {
+            return pixel[x][y];
+        }
+
+        void mudarPixel(int x, int y, char r, char g, char b) {
+            pixel[x][y].r = r;
+            pixel[x][y].g = g;
+            pixel[x][y].b = b;
+        }
+
+        void exibirValoresPixelRGB() {
+            for (int a = 0; a < largura; a++) {
+                cout << "| ";              
+                for (int b = 0; b < altura; b++) {
+                    cout << int(pixel[a][b].r) << int(pixel[a][b].g) << int(pixel[a][b].b) << ", ";
+                }
+                cout << "|\n";
+            }
+        }
+
+        void escreverPPM(string nomeNovoPPM) {
+            ofstream file(nomeNovoPPM);
+            file << tipo << "\n";
+            file << altura << " " << largura << "\n";
+            file << max << "\n";
+            for (int x = 0; x < largura; x++) {
+                for (int y = 0; y < altura; y++) {
+                    file << int(pixel[x][y].r) << " " << int(pixel[x][y].g) << " " << int(pixel[x][y].b) << " ";
+                }
+                file << "\n";
+            }
+        }
+};
---
+++ paleta/paleta.h
@@ -0,0 +1,15 @@
+#ifndef COR_H
+#define COR_H
+
+#include <iostream>
+using namespace std;
+
+class Cor {
+    unsigned char r, g, b;
+
+    public:
+    Cor() : r(0), g(0), b(0) {}
+    Cor (unsigned char _r, unsigned char _g, unsigned char _b) : r(_r), g(_g), b(_b) {}
+};
+
+#endif
